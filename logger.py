5d73474cfba763bbd557076d691de3feaed751cc
social-v2 <nguyenquochuydl123@gmail.com>
2025-07-20 22:08:12 +0700

    Fix logger

--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 os.makedirs("logs", exist_ok=True)
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_filename = f"logs/download_{timestamp}.log"
+log_filename = f"logs/log_{timestamp}.log"
 
 logging.basicConfig(
     level=logging.INFO,
